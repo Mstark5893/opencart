e634e6e4ddb07b3952f645f0df08c8f5fb7d1213
Mstark5893 <manisrai5893@gmail.com>
2024-09-24 22:23:30 +0530

    Adding cart page

---
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+public class CartPage {
+
+    public CartPage(){
+        System.out.println("Cart Page");
+    }
+
+    public void addToCart(){
+        System.out.println("Add to cart page");
+    }
+}
