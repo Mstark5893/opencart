708214651884808424aabc03f913c16ab8262e3d
Mstark5893 <manisrai5893@gmail.com>
2024-09-24 22:34:22 +0530

    Updated cart page

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,6 +1,10 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
+
+    private By cart = By.id("cart");
 
     public CartPage(){
         System.out.println("Cart Page");
